472bd164640f727338358577b24a2ed0b9b7a483
takumi <takumidiary.0927@gmail.com>
2023-09-16 20:19:34 +0900

    create sample api

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjIxOTM3MQ/spectral.js?branch_slug=main&token=fbe9a7d6-e979-4e6b-b665-fcc080d29d0d";
+export default { extends: ruleset };
